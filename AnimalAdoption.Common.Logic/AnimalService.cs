ee6204c1c6b543cf9acfc27f20b3345a3c103f89
Elric Lai <elriclai88@gmail.com>
2022-01-24 14:31:32 +0800

    Changed the placeholder rock information

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Donald Duck", Age = 5, Description = "Duck that can speak human language" },
+            new Animal { Id = 2, Name = "Duckie", Age = 2, Description = "Under a lot of pressure to learn human language" },
+            new Animal { Id = 3, Name = "Igno Yellow Duckling", Age = 1, Description = "Shiny and glasslike. AKA just born" },
         };
     }
 }
